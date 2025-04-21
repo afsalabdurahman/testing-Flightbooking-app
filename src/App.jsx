d066da7c6a2fd94a47066b601ab20e314a43dcfe
Afsal kp <afsalkp4343@gmail.com>
2025-04-21 13:30:12 +0530

    testing Flight

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React, { useState } from 'react';
+
+// import './App.css'
+import FlightBookingPage from '../Component/FlightBookingPage'
+function App() {
+
+
+  return (
+    <>
+      <FlightBookingPage></FlightBookingPage>
+    </>
+  )
+}
+
+export default App
